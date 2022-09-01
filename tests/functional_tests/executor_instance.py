901107fd35b1d44c1468a54778ffc5f99f26ac56
Alejandro Esquivel <ae@alejandro.ltd>
2022-09-01 10:54:47 -0700

    Changed python path to conda env

--- tests/functional_tests/executor_instance.py
+++ tests/functional_tests/executor_instance.py
@@ -6,15 +6,13 @@ import terraform_output
 hostname = os.getenv("SSH_EXECUTOR_HOSTNAME") or terraform_output.get("ec2_public_ip", "")
 username = os.getenv("SSH_EXECUTOR_USERNAME", "ubuntu")
 ssh_key_file = os.getenv("SSH_EXECUTOR_SSH_KEY_FILE", "")
-python_path = os.getenv(
-    "SSH_EXECUTOR_PYTHON_PATH", "/home/ubuntu/miniconda3/envs/covalent/bin/python"
-)
+conda_env = os.getenv("SSH_EXECUTOR_CONDA_ENV", "covalent")
 
 executor_config = {
     "username": username,
     "hostname": hostname,
     "ssh_key_file": ssh_key_file,
-    "python_path": python_path,
+    "conda_env": conda_env,
 }
 
 print("Using Executor Config:")
